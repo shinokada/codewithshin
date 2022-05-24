de85e8bb43ae216c9deea3aa7ae25dadcc1de68c
Shinichi Okada <okada.shin@gmail.com>
2022-05-24 15:31:16 +0900

    fix: update svelte-config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = "production" === "development";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -13,10 +15,13 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		paths: {
+			// change below to your repo name
+			base: dev ? "" : "/codewithshin",
+	},
 	}
 };
 
