d059e291530a4a3e19b9fc9e58fb4f0b61642343
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-12 10:09:45 +0100

    fix: home update

--- src/routes/utils/HeroSection.svelte
+++ src/routes/utils/HeroSection.svelte
@@ -7,17 +7,17 @@
 	<div class="flex flex-col gap-20">
 		<div class="flex items-center gap-8">
 			<div class="flex flex-col justify-start gap-10">
-				<div class="flex flex-col gap-4 text-left align-text-top lg:gap-6">
+				<div class="flex flex-col gap-2 text-left align-text-top xl:gap-6">
 					<h1 class="max-w-2xl text-4xl font-extrabold leading-none text-gray-900 dark:text-white lg:text-5xl mt-2">
 						<span class="block1 xl:inline">Welcome to</span>
 						<span class="block1 text-primary-700 dark:text-primary-700 xl:inline">Codewithshin</span>
 						<span class="block1 xl:inline">where you can find a lot of coding goodies</span>
 					</h1>
-					<p class="text-2xl leading-normal text-gray-500 dark:text-gray-400 lg:text-3xl">
+					<p class="text-2xl leading-normal text-gray-500 dark:text-gray-400 xl:text-3xl">
 						Hi, I'm Shin, a programmer and technology enthusiast with a passion for sharing my knowledge and experience.
 					</p>
-					<div class="mt-4 justify-center sm:flex sm:justify-start md:mt-5">
-						<div class="relative w-full gap-1 text-xl eading-normal text-gray-500 dark:text-gray-400 lg:text-2xl">
+					<div class="mt-4 justify-center sm:flex sm:justify-start md:mt-2">
+						<div class="relative w-full gap-1 text-xl eading-normal text-gray-500 dark:text-gray-400 xl:text-2xl">
 							Please explore my projects and share your thoughts.
 							<div class="mt-4 flex w-full flex-wrap gap-1">
 								<Svelte {size} />
