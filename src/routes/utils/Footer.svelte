99e68066ef6c4a3d008e5be157f2ad9c9b507e64
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-12 16:51:23 +0100

    style: footer update

--- src/routes/utils/Footer.svelte
+++ src/routes/utils/Footer.svelte
@@ -4,7 +4,7 @@
 
 <Footer footerclass="max-w-7xl mx-auto bg-white shadow-none dark:bg-sky-950" footerType="logo">
 	<div class="sm:flex sm:items-center sm:justify-between">
-		<FooterBrand href="/" name="codewithshin.com" />
+		<FooterBrand href="/" name="codewithshin.com" spanclass='text-primary-900 dark:text-primary-500' />
 		<FooterUl
       ulclass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
     >
