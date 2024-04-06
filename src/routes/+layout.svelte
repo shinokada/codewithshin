b20cc9656cec6264cb731b62da201d65e1ceefef
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-04-06 19:33:11 +0200

    fix: google analytics id to env

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,24 @@
 <script>
 	import '../app.pcss';
-	let { children } = $props();
+	let { children, data } = $props();
 	import Nav from './utils/Nav.svelte';
 	import Footer from './utils/Footer.svelte';
+	const analytics = data.ANALYTICS_ID
+  $effect(() => {
+    const script = document.createElement('script');
+    script.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', '${analytics}');
+      `;
+      document.head.appendChild(script);
+    })
 </script>
+
+<svelte:head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id={analytics}"></script>
+</svelte:head>
 
 <Nav />
 
