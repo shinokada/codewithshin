5c49a272da30e977b6834fecf8621099d9ff43d6
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-11-23 13:01:49 +0100

    chore: add /.vercel to gitignore and eslint.config

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.CKPZR4d-.js","_app/immutable/chunks/disclose-version.yjY8Bsl_.js","_app/immutable/chunks/runtime.XeqWOett.js","_app/immutable/chunks/props.CFVCN39j.js","_app/immutable/chunks/theme.BO7MokfJ.js","_app/immutable/chunks/Components.WbO55xFI.js"];
-export const stylesheets = ["_app/immutable/assets/theme.lu8Pa3zS.css"];
-export const fonts = [];
--- .vercel/output/static/_app/immutable/entry/start.zkSqp0Mk.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CHNtCQNg.js";export{t as start};
